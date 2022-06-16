63ba6232058eff187357f120ddb250fd5bed5937
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-06-15 17:14:40 -0700

    Update macrophage.py

--- macrophage.py
+++ macrophage.py
@@ -3,7 +3,7 @@ from glob import glob
 import matplotlib.pyplot as plt
 import imageio as io
 from image_operations import sort_nicely
-from skimage.morphology import remove_small_objects, binary_erosion, binary_opening
+from skimage.morphology import remove_small_objects, binary_erosion, binary_opening, binary_dilation
 from scipy.ndimage import binary_fill_holes
 from skimage.measure import label, regionprops
 
@@ -33,7 +33,7 @@ def find_masks(array):
         small_objects_removed_mask = np.array(remove_small_objects(bool_mask, 2200, in_place=True), int) 
         binary_opening(small_objects_removed_mask)       # Remove noise
         binary_fill_holes(small_objects_removed_mask)    # Close any holes from image opening
-        cleaned_mask = binary_erosion(small_objects_removed_mask)
+        cleaned_mask = binary_dilation(binary_erosion(small_objects_removed_mask))      # Dilation and erosion of image
         images_lst.append(cleaned_mask)
 
     images_array = np.asarray(images_lst)
@@ -49,22 +49,38 @@ def get_regions(array):
     regions = regionprops(labeled_mask)
     return regions
 
-"""
-TO DO:
-def show_subset_image(array):
-    return
-"""
+def show_subset_image(image_array, masks_array, regions_array):
+    for idx, image in enumerate(image_array):
+        for region in regions_array:
+            coord = region.coords
+
+                plt.figure()
+        
+                plt.imshow(image_array[idx][0:(coord[0]+300), (coord[1]+100):2000], cmap='gray')
+                plt.imshow(masks_array[idx][0:(coord[0]+300), (coord[1]+100):2000], alpha=0.2, cmap='gray_r')
+
 
 
 def main():
     GFP_image_array = load_images("C:/Pos1_/Pos1/zStack/GFP/Default/*.*")
-    RFP_image_array = load_images("C:/Pos1_/Pos1/zStack/RFP/Default/*.*")
+    # RFP_image_array = load_images("C:/Pos1_/Pos1/zStack/RFP/Default/*.*")
 
     GFP_masks_array = find_masks(GFP_image_array)
-    RFP_masks_array = find_masks(RFP_image_array)
+    # RFP_masks_array = find_masks(RFP_image_array)
 
-    mask_intersection = product_of_masks(GFP_masks_array, RFP_masks_array)
-    regions = get_regions(mask_intersection)
+    # plt.figure()
+    # plt.imshow(GFP_image_array[0], cmap='gray')
+
+    # plt.figure()
+    # plt.imshow(GFP_image_array[0], cmap='gray')
+    # plt.imshow(GFP_masks_array[0], alpha=0.2, cmap='gray_r')
+
+
+    # mask_intersection = product_of_masks(GFP_masks_array, RFP_masks_array)
+    # regions = get_regions(mask_intersection)
+    # show_subset_image(GFP_image_array, regions)
+
+
 
 
 
